eb759583f0efdc6e8be618eb05860b35a171fae6
ilenceye <ye66169@gmail.com>
2025-05-05 15:08:38 +0800

    add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import React from "react";
+
 import ReactDOM from "react-dom/client";
+
 import App from "./app";
+import "./index.css";
 import { createTray } from "./tray";
 
 createTray();
@@ -8,5 +11,5 @@ createTray();
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
